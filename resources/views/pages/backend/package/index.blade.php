8c156a501b870f0087b130df37b084b489de3cfb
Youcef Nezrek <98532488+KaziSTM@users.noreply.github.com>
2024-11-08 23:21:55 +0100

    Update index.blade.php

--- resources/views/pages/backend/package/index.blade.php
+++ resources/views/pages/backend/package/index.blade.php
@@ -6,7 +6,7 @@
         <div class="flex space-x-4">
             <x-button text="{!! trans('Add') !!}" icon="plus" position="right" primary outline
                       wire:click="redirectToCreatePage"/>
-            <x-button text="{!! trans('Export Csv') !!}" icon="export" position="right" color="secondary" outline
+            <x-button text="{!! trans('Export') !!}" icon="export" position="right" color="secondary" outline
                       wire:click="exportExcel"/>
         </div>
     </x-slot>
